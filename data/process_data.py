dcdbd289115b8f2f7306492594cb678cf5db8cce
wintor214 <85626677+wintor214@users.noreply.github.com>
2021-10-02 09:33:12 -0400

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -56,7 +56,7 @@ def save_data(df, database_filename):
     '''
     #engine = create_engine('sqlite:///DisasterResponse.db')
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('data', engine, index=False)
+    df.to_sql('data', engine, index=False,if_exists = 'replace')
 
 def main():
     if len(sys.argv) == 4:
